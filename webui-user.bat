c6340343fce2934ed3450eab8dd0067ff619b32e
neorack <cumin_murky.0a@icloud.com>
2024-12-20 06:38:20 -0700

    Add webui configuration files and update package dependencies

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,14 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS=--xformers ^
+  --models-dir="../Models" ^
+  --ckpt-dir="../Models/checkpoints" ^
+  --vae-dir="../Models/vae" ^
+  --lora-dir="../Models/Lora" ^
+  --embeddings-dir="../Models/embeddings" ^
+  --hypernetwork-dir="../Models/hypernetworks" ^
+  --no-half-vae ^
+  --medvram
 
 call webui.bat
